8f86c2b9fa9c2ecaa6e8d1dbe92d71f24d2f90ef
Grayroof <1grayroof@gmail.com>
2022-12-13 10:14:58 +0300

    style: добавить разделяющие строки

--- src/main/java/ru/practicum/shareit/user/UserStorage.java
+++ src/main/java/ru/practicum/shareit/user/UserStorage.java
@@ -5,9 +5,14 @@ import ru.practicum.shareit.user.model.User;
 import java.util.Collection;
 
 public interface UserStorage {
+
     User get(long id);
+
     Collection<User> getAll();
+
     User add(User user);
+
     User patch(User user);
+
     Boolean delete(long id);
 }
