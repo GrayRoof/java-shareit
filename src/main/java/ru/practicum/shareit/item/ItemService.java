b11649b9294e5eb00b101f1dac2b051c1c58b09f
Grayroof <1grayroof@gmail.com>
2022-12-13 10:46:49 +0300

    style: добавить разделяющие строки

--- src/main/java/ru/practicum/shareit/item/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -5,10 +5,16 @@ import ru.practicum.shareit.item.dto.ItemDto;
 import java.util.Collection;
 
 public interface ItemService {
+
     ItemDto get(long id);
+
     Collection<ItemDto> getAllByUserId(long userId);
+
     ItemDto add(ItemDto itemDto, long ownerId);
+
     ItemDto patch(ItemDto itemDto, long itemId, long userId);
+
     boolean delete(long id);
+
     Collection<ItemDto> search(String text, long userId);
 }
