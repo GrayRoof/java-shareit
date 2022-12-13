7e3d2c745d9baa9dba904b6a64caa9f0341a227b
Grayroof <1grayroof@gmail.com>
2022-12-13 10:09:39 +0300

    style: добавить перенос строки

--- src/main/java/ru/practicum/shareit/Exception/DuplicateEmailException.java
+++ src/main/java/ru/practicum/shareit/Exception/DuplicateEmailException.java
@@ -1,5 +1,7 @@
 package ru.practicum.shareit.Exception;
 
 public class DuplicateEmailException extends RuntimeException {
-    public DuplicateEmailException(String message) {super(message);}
+    public DuplicateEmailException(String message) {
+        super(message);
+    }
 }
