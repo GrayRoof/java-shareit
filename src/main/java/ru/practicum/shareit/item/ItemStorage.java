cffe605ae56fa69a2addcaf85621657c286d21ca
Grayroof <1grayroof@gmail.com>
2022-12-13 12:32:02 +0300

    style: добавить разделяющие строки

--- src/main/java/ru/practicum/shareit/item/ItemStorage.java
+++ src/main/java/ru/practicum/shareit/item/ItemStorage.java
@@ -5,10 +5,16 @@ import ru.practicum.shareit.item.model.Item;
 import java.util.Collection;
 
 public interface ItemStorage {
+
     Item get(long id);
+
     Collection<Item> getAllByOwnerId(long ownerId);
+
     Item add(Item item);
+
     Item patch(Item item);
+
     boolean delete(long id);
+
     Collection<Item> search(String search, long userId);
 }
