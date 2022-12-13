1e218b766cf2797d868a2d45a71217a10a46cd2e
Grayroof <1grayroof@gmail.com>
2022-12-13 10:20:54 +0300

    style: добавить разделяющие строки

--- src/main/java/ru/practicum/shareit/user/UserService.java
+++ src/main/java/ru/practicum/shareit/user/UserService.java
@@ -5,9 +5,14 @@ import ru.practicum.shareit.user.dto.UserDto;
 import java.util.Collection;
 
 public interface UserService {
+
     UserDto get(long id);
+
     Collection<UserDto> getAll();
+
     UserDto add(UserDto userDto);
+
     UserDto patch(UserDto userDto, long id);
+
     Boolean delete(long id);
 }
