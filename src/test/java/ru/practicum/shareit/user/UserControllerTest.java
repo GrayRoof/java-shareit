6512833a2fa7d30a8b4dcce271f1a2d621bc47bb
Grayroof <1grayroof@gmail.com>
2023-01-15 22:38:34 +0300

    feat: добавить юнит тест getUserById

---
+++ src/test/java/ru/practicum/shareit/user/UserControllerTest.java
@@ -0,0 +1,74 @@
+package ru.practicum.shareit.user;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.dto.UserMapper;
+import ru.practicum.shareit.user.model.User;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = UserController.class)
+class UserControllerTest {
+    @Autowired
+    private ObjectMapper mapper;
+    @MockBean
+    private UserService userService;
+    @Autowired
+    private MockMvc mvc;
+
+    User user;
+    UserDto userToUpdateDto;
+    UserDto userDto;
+
+    @BeforeEach
+    void setUp() {
+        user = new User(
+                1L,
+                "First",
+                "First@test.test");
+        userDto = UserMapper.toUserDto(user);
+        userToUpdateDto = new UserDto(
+                1L,
+                "Updated",
+                "First@test.test");
+    }
+
+    @Test
+    void getUserById() throws Exception {
+        when(userService.get(anyLong()))
+                .thenReturn(userDto);
+        mvc.perform(get("/users/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(userDto.getId()), Long.class))
+                .andExpect(jsonPath("$.name", is(userDto.getName())))
+                .andExpect(jsonPath("$.email", is(userDto.getEmail())));
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
