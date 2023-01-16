0cb5837fc94a3e29effb3a215416c8f620cc9b7b
Grayroof <1grayroof@gmail.com>
2023-01-16 08:53:56 +0300

    feat: добавить юнит тесты create, update, delete

--- src/test/java/ru/practicum/shareit/user/UserControllerTest.java
+++ src/test/java/ru/practicum/shareit/user/UserControllerTest.java
@@ -6,15 +6,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.dto.UserMapper;
 import ru.practicum.shareit.user.model.User;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -48,7 +50,7 @@ class UserControllerTest {
     }
 
     @Test
-    void getUserById() throws Exception {
+    void shouldCallGetUserById() throws Exception {
         when(userService.get(anyLong()))
                 .thenReturn(userDto);
         mvc.perform(get("/users/1"))
@@ -59,7 +61,7 @@ class UserControllerTest {
     }
 
     @Test
-    void getAllUsers() throws Exception {
+    void shouldCallGetAllUsers() throws Exception {
         when(userService.getAll())
                 .thenReturn(List.of(userDto));
         mvc.perform(get("/users"))
@@ -68,14 +70,36 @@ class UserControllerTest {
     }
 
     @Test
-    void create() {
+    void shouldCallCreate() throws Exception {
+        when(userService.add(any())).thenReturn(userDto);
+        mvc.perform(
+                    post("/users")
+                    .content(mapper.writeValueAsString(userDto))
+                    .characterEncoding(StandardCharsets.UTF_8)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(userDto.getId()), Long.class));
+
     }
 
     @Test
-    void update() {
+    void shouldCallUpdate() throws Exception {
+        when(userService.patch(any(), anyLong())).thenReturn(userToUpdateDto);
+        mvc.perform(patch("/users/1")
+                    .content(mapper.writeValueAsString(userToUpdateDto))
+                    .characterEncoding(StandardCharsets.UTF_8)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(userToUpdateDto.getId()), Long.class))
+                .andExpect(jsonPath("$.name", is(userToUpdateDto.getName())));
     }
 
     @Test
-    void delete() {
+    void shouldCallDelete() throws Exception {
+        mvc.perform(delete("/users/1")).andExpect(status().isOk());
     }
 }
