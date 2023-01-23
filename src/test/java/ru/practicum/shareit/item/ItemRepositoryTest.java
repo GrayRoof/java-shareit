5ed6a4906ed61638de203f88cab4261389b733e8
Grayroof <1grayroof@gmail.com>
2023-01-23 19:46:43 +0300

    test: добавить класс для интеграционных тестов для ItemRepository

---
+++ src/test/java/ru/practicum/shareit/item/ItemRepositoryTest.java
@@ -0,0 +1,35 @@
+package ru.practicum.shareit.item;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class ItemRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void search() {
+    }
+
+    @Test
+    void findAllByOwner_IdOrderByIdAsc() {
+    }
+
+    @Test
+    void findAllByRequest() {
+    }
+
+    @Test
+    void findByIdOrderByIdDesc() {
+    }
+}
