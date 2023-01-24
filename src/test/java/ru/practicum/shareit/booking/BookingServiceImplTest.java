30724b734e3b66336e486b47d58cd831cbdd1fe7
Grayroof <1grayroof@gmail.com>
2023-01-24 12:59:02 +0300

    test: добавить тесты исключений

--- src/test/java/ru/practicum/shareit/booking/BookingServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/booking/BookingServiceImplTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.practicum.shareit.Exception.NotAvailableException;
 import ru.practicum.shareit.Exception.NotFoundException;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.BookingMapper;
@@ -50,11 +51,11 @@ class BookingServiceImplTest {
 
     private static final long WRONG_ID = Long.MAX_VALUE;
 
-    UserDto owner;
-    UserDto otherUser;
+    UserDto ownerDto;
+    UserDto otherUserDto;
 
-    ItemAllFieldsDto firstItem;
-    ItemAllFieldsDto secondItem;
+    ItemAllFieldsDto firstItemDto;
+    ItemAllFieldsDto secondItemDto;
 
 
     @BeforeEach
@@ -63,163 +64,212 @@ class BookingServiceImplTest {
         entityManager.createNativeQuery("ALTER TABLE bookings ALTER COLUMN id RESTART WITH 1").executeUpdate();
         entityManager.createQuery("DELETE FROM Item").executeUpdate();
         if (userService.getAll().isEmpty()) {
-            owner = userService.add(UserMapper.toUserDto(new User(1L, "Owner", "Owner@test.test")));
-            otherUser = userService.add(UserMapper.toUserDto(new User(2L, "Other", "Other@test.test")));
+            ownerDto = userService.add(UserMapper.toUserDto(new User(1L, "Owner", "Owner@test.test")));
+            otherUserDto = userService.add(UserMapper.toUserDto(new User(2L, "Other", "Other@test.test")));
         }
-        if (itemService.getAllByUserId(owner.getId(), 0, 20).isEmpty()) {
+        if (itemService.getAllByUserId(ownerDto.getId(), 0, 20).isEmpty()) {
             ItemAllFieldsDto dtoToInput = new ItemAllFieldsDto();
             dtoToInput.setName("First Item");
             dtoToInput.setDescription("First Item Description");
             dtoToInput.setAvailable(true);
-            firstItem = itemService.add(dtoToInput, owner.getId());
+            firstItemDto = itemService.add(dtoToInput, ownerDto.getId());
             dtoToInput.setName("Second Item");
             dtoToInput.setDescription("Second Item Description");
-            dtoToInput.setAvailable(true);
-            secondItem = itemService.add(dtoToInput, owner.getId());
+            dtoToInput.setAvailable(false);
+            secondItemDto = itemService.add(dtoToInput, ownerDto.getId());
         }
     }
 
     @Test
     void shouldReturnBookingDto() {
         Booking booking = new Booking();
-        Item bookingItem = ItemMapper.toItem(firstItem, UserMapper.toUser(owner));
-        bookingItem.setId(firstItem.getId());
+        Item bookingItem = ItemMapper.toItem(firstItemDto, UserMapper.toUser(ownerDto));
+        bookingItem.setId(firstItemDto.getId());
         booking.setItem(bookingItem);
-        booking.setBooker(UserMapper.toUser(otherUser));
+        booking.setBooker(UserMapper.toUser(otherUserDto));
         booking.setStart(LocalDateTime.now().plusDays(1));
         booking.setEnd(LocalDateTime.now().plusDays(2));
         booking.setStatus(BookingStatus.APPROVED);
         Booking saved = bookingRepository.save(booking);
         BookingDto expected = BookingMapper.toBookingDto(saved);
-        BookingDto actual = bookingService.get(otherUser.getId(), saved.getId());
+        BookingDto actual = bookingService.get(otherUserDto.getId(), saved.getId());
         assertEquals(expected, actual);
     }
 
     @Test
     void shouldThrowExceptionWhenGetWithWrongId() {
         Booking booking = new Booking();
-        Item bookingItem = ItemMapper.toItem(firstItem, UserMapper.toUser(owner));
-        bookingItem.setId(firstItem.getId());
+        Item bookingItem = ItemMapper.toItem(firstItemDto, UserMapper.toUser(ownerDto));
+        bookingItem.setId(firstItemDto.getId());
         booking.setItem(bookingItem);
-        booking.setBooker(UserMapper.toUser(otherUser));
+        booking.setBooker(UserMapper.toUser(otherUserDto));
         booking.setStart(LocalDateTime.now().plusDays(1));
         booking.setEnd(LocalDateTime.now().plusDays(2));
         booking.setStatus(BookingStatus.APPROVED);
         bookingRepository.save(booking);
-        assertThrows(NotFoundException.class, () -> bookingService.get(otherUser.getId(), WRONG_ID));
+        assertThrows(NotFoundException.class, () -> bookingService.get(otherUserDto.getId(), WRONG_ID));
     }
 
     @Test
     void shouldReturnLastForItem() {
         Booking booking = new Booking();
-        Item bookingItem = ItemMapper.toItem(firstItem, UserMapper.toUser(owner));
-        bookingItem.setId(firstItem.getId());
+        Item bookingItem = ItemMapper.toItem(firstItemDto, UserMapper.toUser(ownerDto));
+        bookingItem.setId(firstItemDto.getId());
         booking.setItem(bookingItem);
-        booking.setBooker(UserMapper.toUser(otherUser));
+        booking.setBooker(UserMapper.toUser(otherUserDto));
         booking.setStart(LocalDateTime.now().minusDays(5));
         booking.setEnd(LocalDateTime.now().minusDays(2));
         booking.setStatus(BookingStatus.APPROVED);
         Booking saved = bookingRepository.save(booking);
         BookingNestedDto expected = BookingMapper.toBookingNestedDto(saved);
-        BookingNestedDto actual = bookingService.getLastForItem(firstItem.getId());
+        BookingNestedDto actual = bookingService.getLastForItem(firstItemDto.getId());
         assertEquals(expected, actual);
     }
 
     @Test
     void shouldReturnNextForItem() {
         Booking booking = new Booking();
-        Item bookingItem = ItemMapper.toItem(firstItem, UserMapper.toUser(owner));
-        bookingItem.setId(firstItem.getId());
+        Item bookingItem = ItemMapper.toItem(firstItemDto, UserMapper.toUser(ownerDto));
+        bookingItem.setId(firstItemDto.getId());
         booking.setItem(bookingItem);
-        booking.setBooker(UserMapper.toUser(otherUser));
+        booking.setBooker(UserMapper.toUser(otherUserDto));
         booking.setStart(LocalDateTime.now().plusDays(5));
         booking.setEnd(LocalDateTime.now().plusDays(8));
         booking.setStatus(BookingStatus.APPROVED);
         Booking saved = bookingRepository.save(booking);
         BookingNestedDto expected = BookingMapper.toBookingNestedDto(saved);
-        BookingNestedDto actual = bookingService.getNextForItem(firstItem.getId());
+        BookingNestedDto actual = bookingService.getNextForItem(firstItemDto.getId());
         assertEquals(expected, actual);
     }
 
     @Test
     void shouldReturnCreated() {
         Booking booking = new Booking();
-        Item bookingItem = ItemMapper.toItem(firstItem, UserMapper.toUser(owner));
-        bookingItem.setId(firstItem.getId());
+        Item bookingItem = ItemMapper.toItem(firstItemDto, UserMapper.toUser(ownerDto));
+        bookingItem.setId(firstItemDto.getId());
         booking.setItem(bookingItem);
-        booking.setBooker(UserMapper.toUser(otherUser));
+        booking.setBooker(UserMapper.toUser(otherUserDto));
         booking.setStart(LocalDateTime.now().plusDays(5));
         booking.setEnd(LocalDateTime.now().plusDays(8));
         booking.setStatus(BookingStatus.APPROVED);
         Booking saved = bookingRepository.save(booking);
         Collection<BookingDto> expected = List.of(BookingMapper.toBookingDto(saved));
-        Collection<BookingDto> actual = bookingService.getCreated(otherUser.getId(), "FUTURE", 0, 20);
+        Collection<BookingDto> actual = bookingService.getCreated(otherUserDto.getId(), "FUTURE", 0, 20);
         assertEquals(expected, actual);
     }
 
     @Test
     void shouldReturnForOwnedItems() {
         Booking booking = new Booking();
-        Item bookingItem = ItemMapper.toItem(firstItem, UserMapper.toUser(owner));
-        bookingItem.setId(firstItem.getId());
+        Item bookingItem = ItemMapper.toItem(firstItemDto, UserMapper.toUser(ownerDto));
+        bookingItem.setId(firstItemDto.getId());
         booking.setItem(bookingItem);
-        booking.setBooker(UserMapper.toUser(otherUser));
+        booking.setBooker(UserMapper.toUser(otherUserDto));
         booking.setStart(LocalDateTime.now().plusDays(5));
         booking.setEnd(LocalDateTime.now().plusDays(8));
         booking.setStatus(BookingStatus.APPROVED);
         Booking saved = bookingRepository.save(booking);
         Collection<BookingDto> expected = List.of(BookingMapper.toBookingDto(saved));
-        Collection<BookingDto> actual = bookingService.getForOwnedItems(owner.getId(), "FUTURE", 0, 20);
+        Collection<BookingDto> actual = bookingService.getForOwnedItems(ownerDto.getId(), "FUTURE", 0, 20);
         assertEquals(expected, actual);
     }
 
     @Test
     void shouldReturnFinishedCount() {
         Booking booking = new Booking();
-        Item bookingItem = ItemMapper.toItem(firstItem, UserMapper.toUser(owner));
-        bookingItem.setId(firstItem.getId());
+        Item bookingItem = ItemMapper.toItem(firstItemDto, UserMapper.toUser(ownerDto));
+        bookingItem.setId(firstItemDto.getId());
         booking.setItem(bookingItem);
-        booking.setBooker(UserMapper.toUser(otherUser));
+        booking.setBooker(UserMapper.toUser(otherUserDto));
         booking.setStart(LocalDateTime.now().minusDays(5));
         booking.setEnd(LocalDateTime.now().minusDays(3));
         booking.setStatus(BookingStatus.APPROVED);
         bookingRepository.save(booking);
-        int actual = bookingService.getFinishedCount(otherUser.getId(), firstItem.getId());
+        int actual = bookingService.getFinishedCount(otherUserDto.getId(), firstItemDto.getId());
         assertEquals(1, actual);
     }
 
     @Test
     void shouldCreate() {
         BookingToInputDto toInputDto = new BookingToInputDto();
-        toInputDto.setItemId(firstItem.getId());
+        toInputDto.setItemId(firstItemDto.getId());
         toInputDto.setStart(LocalDateTime.now().plusDays(1));
         toInputDto.setEnd(LocalDateTime.now().plusDays(2));
-        Item item = ItemMapper.toItem(firstItem, UserMapper.toUser(owner));
-        item.setId(firstItem.getId());
+        Item item = ItemMapper.toItem(firstItemDto, UserMapper.toUser(ownerDto));
+        item.setId(firstItemDto.getId());
         Booking newBooking = BookingMapper.toBooking(
                 toInputDto,
-                UserMapper.toUser(otherUser),
+                UserMapper.toUser(otherUserDto),
                 item);
-        BookingDto actual = bookingService.create(otherUser.getId(), toInputDto);
+        BookingDto actual = bookingService.create(otherUserDto.getId(), toInputDto);
         BookingDto expected = BookingMapper.toBookingDto(newBooking);
         expected.setId(actual.getId());
         assertEquals(expected, actual);
     }
 
     @Test
+    void shouldThrowExceptionWhenOwnerTryToCreate() {
+        BookingToInputDto toInputDto = new BookingToInputDto();
+        toInputDto.setItemId(firstItemDto.getId());
+        toInputDto.setStart(LocalDateTime.now().plusDays(1));
+        toInputDto.setEnd(LocalDateTime.now().plusDays(2));
+        assertThrows(NotFoundException.class, () -> bookingService.create(ownerDto.getId(), toInputDto));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenTryCreateForNotAvailableItem() {
+        BookingToInputDto toInputDto = new BookingToInputDto();
+        toInputDto.setItemId(secondItemDto.getId());
+        toInputDto.setStart(LocalDateTime.now().plusDays(1));
+        toInputDto.setEnd(LocalDateTime.now().plusDays(2));
+
+        assertThrows(NotAvailableException.class, () -> bookingService.create(otherUserDto.getId(), toInputDto));
+    }
+
+    @Test
     void shouldSetApproved() {
         Booking booking = new Booking();
-        Item bookingItem = ItemMapper.toItem(firstItem, UserMapper.toUser(owner));
-        bookingItem.setId(firstItem.getId());
+        Item bookingItem = ItemMapper.toItem(firstItemDto, UserMapper.toUser(ownerDto));
+        bookingItem.setId(firstItemDto.getId());
         booking.setItem(bookingItem);
-        booking.setBooker(UserMapper.toUser(otherUser));
+        booking.setBooker(UserMapper.toUser(otherUserDto));
         booking.setStart(LocalDateTime.now().minusDays(5));
         booking.setEnd(LocalDateTime.now().minusDays(3));
         booking.setStatus(BookingStatus.WAITING);
         Booking saved = bookingRepository.save(booking);
         BookingDto expected = BookingMapper.toBookingDto(saved);
         expected.setStatus(BookingStatus.APPROVED);
-        BookingDto actual = bookingService.setApproved(owner.getId(), saved.getId(), true);
+        BookingDto actual = bookingService.setApproved(ownerDto.getId(), saved.getId(), true);
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenTrySetApprovedByNotOwnerOfItem() {
+        Booking booking = new Booking();
+        Item bookingItem = ItemMapper.toItem(firstItemDto, UserMapper.toUser(ownerDto));
+        bookingItem.setId(firstItemDto.getId());
+        booking.setItem(bookingItem);
+        booking.setBooker(UserMapper.toUser(otherUserDto));
+        booking.setStart(LocalDateTime.now().minusDays(5));
+        booking.setEnd(LocalDateTime.now().minusDays(3));
+        booking.setStatus(BookingStatus.WAITING);
+        Booking saved = bookingRepository.save(booking);
+        assertThrows(NotFoundException.class, () -> bookingService
+                .setApproved(otherUserDto.getId(), saved.getId(), true));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenTrySetApprovedToApprovedBooking() {
+        Booking booking = new Booking();
+        Item bookingItem = ItemMapper.toItem(firstItemDto, UserMapper.toUser(ownerDto));
+        bookingItem.setId(firstItemDto.getId());
+        booking.setItem(bookingItem);
+        booking.setBooker(UserMapper.toUser(otherUserDto));
+        booking.setStart(LocalDateTime.now().minusDays(5));
+        booking.setEnd(LocalDateTime.now().minusDays(3));
+        booking.setStatus(BookingStatus.APPROVED);
+        Booking saved = bookingRepository.save(booking);
+        assertThrows(NotAvailableException.class, () -> bookingService
+                .setApproved(ownerDto.getId(), saved.getId(), true));
     }
 }
